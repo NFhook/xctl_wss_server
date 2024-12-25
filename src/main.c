65858010ecbba465b85ebd7121df9c3515bdd244
NFhook <telegant@qq.com>
2024-12-25 18:00:24 +0800

    support log

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <libwebsockets.h>
 #include "server.h"
+#include "log.h"
 
 static int interrupted = 0;
 
@@ -22,6 +23,15 @@ int main(int argc, char **argv) {
         {NULL, NULL, 0, 0}
     };
 
+    logger_init("xcc", LOG_DEBUG);
+    logger_set_log_file("app.log");
+
+
+    LOG_INFO("Application started");
+    LOG_DEBUG("Debug information: %d", 42);
+    LOG_WARN("Warning message");
+    LOG_ERROR("Error occurred: %s", "Something went wrong");
+
     memset(&info, 0, sizeof(info));
     info.port = 8080;
     info.protocols = protocols;
@@ -35,17 +45,17 @@ int main(int argc, char **argv) {
     lwsl_notice("WebSocket server started on port 8080\n");
 
     signal(SIGINT, sigint_handler);
-    
+
     lwsl_user("Starting server on port: %d\n", info.port);
-    
-    
+
+
     //while (lws_service(context, 1000) >= 0);
     while(!interrupted) {
-	lws_service(context, 1000);
+	    lws_service(context, 1000);
     }
 
     lws_context_destroy(context);
-    
+    logger_destroy();
     lwsl_user("Server stoped!\n");
     return 0;
 }
